755101a1b8b8b8ac9f5ee36e8ed7bde8ef38816f
Henrique Liberato <henriquebmx@gmail.com>
2020-12-11 02:30:07 -0300

    Create router structure and home container

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    Icons made by <a href="https://www.flaticon.com/authors/roundicons-freebies" title="Roundicons Freebies">Roundicons Freebies</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-header
+      :class="[{'center': isAtSearchForm}, isAtSearchForm ? 'el-header__home' : 'el-header__content']"
+      class="pt-20 pb-20 flex"
+      height="auto"
+    >
+      <div
+        class="flex flex-row vertical-center cursor-pointer"
+        @click="returnToHome"
+      >
+        <pokemon-icon :size="isAtSearchForm ? 'large' : 'medium'" />
+        <h1 :class="isAtSearchForm ? 'ml-28' : 'ml-12'">
+          PokeGames Search!
+        </h1>
+      </div>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer class="text-center">
+      © PokeGames Search! - 2020
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  computed: {
+    isAtSearchForm () {
+      return this.$route.name === 'SearchForm'
+    }
+  },
+  methods: {
+    returnToHome () {
+      if (this.isAtSearchForm) this.$router.push({ name: 'Home' })
+    }
   }
 }
 </script>
